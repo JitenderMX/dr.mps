782291fa7c3b1663edbc0c75a51d0bc451e8e9ae
JitenderMX <jitender.work.mediax@gmail.com>
2024-06-05 16:38:23 +0530

    Admissions and Training page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Loading Common Element Using External File
 $("#header").load("header.html");
 $("#footer").load("footer.html");
+$("#faq").load("faq.html");
+$("#admission-year").load("admission-year.html");
 
 
 
